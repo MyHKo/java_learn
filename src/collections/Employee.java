236d94ba96c6363ab0fe2186f5c5761e1405ee31
Mykola Horbenko <bfly@fedora.home>
2025-12-09 16:52:11 +0100

    Create a test class

---
+++ src/collections/Employee.java
@@ -0,0 +1,20 @@
+package collections;
+
+public class Employee {
+	String name;
+    String department;
+    double salary;
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public double getSalary() { return salary; }
+    
+    @Override
+    public String toString() { return name + " ($" + salary + ")"; }
+}
---
+++ src/collections/RichList.java
@@ -0,0 +1,18 @@
+package collections;
+import java.util.*;
+
+public class RichList {
+	public static void main(String[] args) {
+		List<Employee> workers = Arrays.asList(
+				new Employee("Alice", "IT", 75000),
+	            new Employee("Bob", "HR", 50000),
+	            new Employee("Charlie", "IT", 80000),
+	            new Employee("David", "Sales", 45000),
+	            new Employee("Eve", "Sales", 90000),
+	            new Employee("Frank", "IT", 75000)
+	            );
+		
+		
+	
+	}
+}
